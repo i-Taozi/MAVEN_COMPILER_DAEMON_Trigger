ecc296768b59f9cc5128b95fd14fc3cfd64f4a31
i-Taozi <879714748@qq.com>
2022-02-19 06:21:18 +0800

    jzy3d/jzy3d-api:original code

--- persistence-api/src/main/java/javax/persistence/criteria/CompoundSelection.java
+++
@@ -1,9 +0,0 @@
-package javax.persistence.criteria;
-
-/**
- * The CompoundSelection interface defines a compound selection item (tuple, array, or result of constructor).
- * 
- * @param <X>
- *            the type of the selection item
- */
-public interface CompoundSelection<X> extends Selection<X> {}
--- persistence-api/src/main/java/javax/persistence/metamodel/MappedSuperclassType.java
+++
@@ -1,9 +0,0 @@
-package javax.persistence.metamodel;
-
-/**
- * Instances of the type MappedSuperclassType represent mapped superclass types.
- * 
- * @param <X>
- *            The represented entity type
- */
-public interface MappedSuperclassType<X> extends IdentifiableType<X> {}
