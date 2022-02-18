fe0176b77c7b9eb309d32e091d7f68b878c74897
i-Taozi <879714748@qq.com>
2022-02-19 07:40:17 +0800

    v5developer/maven-framework-project:original code

--- tritonserver/src/main/java9/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.bytedeco.tritonserver {
-  requires transitive org.bytedeco.javacpp;
-  requires transitive org.bytedeco.cuda;
-  requires transitive org.bytedeco.tensorrt;
-  exports org.bytedeco.tritonserver.global;
-  exports org.bytedeco.tritonserver.presets;
-  exports org.bytedeco.tritonserver;
-}
